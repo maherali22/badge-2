7330fa5075bf00037a4dddc7ca8b1a2140b5a43f
Maher <mahiralimattummal@gmail.com>
2024-12-18 23:55:49 +0530

    update

--- javascript/3.nestedObj.js
+++ javascript/3.nestedObj.js
@@ -15,5 +15,16 @@ console.log(`Company: ${companyName}`);  // Output: Company: Bridgeon
 console.log(`City: ${city}`);            // Output: City: Malappuram
 console.log(`State: ${state}`);          // Output: State: Kerala
 console.log(`Employees: ${employees}`); // Output: Employees: 50
-console.log(companyName);
-console.log(city);
+
+// Defining an object
+const person = {
+  name: "Maher Ali",
+  age: 25,
+  location: "Malappuram",
+  skills: ["HTML", "CSS", "JavaScript", "React"]
+};
+
+// Accessing object properties
+console.log(person.name);      // Output: Maher Ali
+console.log(person.age);       // Output: 25
+console.log(person.skills[2]); // Output: JavaScript

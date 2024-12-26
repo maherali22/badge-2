64a4e76949cbc3bea5849616861f52e2dab82f31
Maher <mahiralimattummal@gmail.com>
2024-12-26 21:29:58 +0530

    update

--- leetcode2.js
+++ leetcode2.js
@@ -17,3 +17,35 @@ var lengthOfLastWord = function(s) {
     }
     return lastWord.length;
 };
+
+var lengthOfLastWord = function(s) {
+    let lastWord = "";
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (s[i] === " ") {
+            if (lastWord.length > 0) {
+                return lastWord.length;
+            }
+        } else {
+            lastWord = s[i] + lastWord;
+        }
+    }
+    return lastWord.length;
+};
+
+var palindrome = function(s) {
+    let reversed = "";
+    for (let i = s.length - 1; i >= 0; i--) {
+        reversed += s[i];
+    }
+    return reversed === s;
+};
+
+var camelCase = function(s) {
+    let count = 1;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === s[i].toUpperCase()) {
+            count++;
+        }
+    }
+    return count;
+};
